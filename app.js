f59998cc952a1232cc6cb78d7077e6d4a28ac8fc
vikas-bareth <iamvikasbareth@gmail.com>
2023-07-15 10:54:47 +0530

    added basic maths functions and variables

---
+++ app.js
@@ -0,0 +1,18 @@
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
+let shouldResetScreen = false;
+
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+
