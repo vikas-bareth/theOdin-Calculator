6b5fdee7443b8a32465543edaef188539dc0b75e
vikas-bareth <iamvikasbareth@gmail.com>
2023-07-16 01:41:25 +0530

    completed ui with styles and html

--- app.js
+++ app.js
@@ -16,3 +16,20 @@ function divide(a,b){
     return a/b;
 }
 
+function operate(operator,a,b){
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+          return add(a, b)
+        case '-':
+          return substract(a, b)
+        case '*':
+          return multiply(a, b)
+        case '/':
+          if (b === 0) return null
+          else return divide(a, b)
+        default:
+          return null
+      }
+}
